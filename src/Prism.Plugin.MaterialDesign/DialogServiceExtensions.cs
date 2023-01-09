b3171b389c9f83fe9d85cc0ebe2869629a3182b3
Federico Ruggiero <feddstylez@gmail.com>
2023-01-09 18:38:03 +0100

    Scaffold

---
+++ src/Prism.Plugin.MaterialDesign/DialogServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace Prism.Services.Dialogs
+{
+    public static class DialogServiceExtensions
+    {
+        /// <summary>
+        /// Shows a modal dialog using a <see cref="MaterialDesignThemes.Wpf.DialogHost"/>.
+        /// </summary>
+        /// <param name="dialogService"></param>
+        /// <param name="name">The name of the dialog to show.</param>
+        /// <param name="parameters">The parameters to pass to the dialog.</param>
+        /// <param name="callback">The action to perform when the dialog is closed.</param>
+        /// <exception cref="NullReferenceException">Thrown when the dialog service is not a MaterialDialogService</exception>
+        public static void ShowDialogHost(this IDialogService dialogService, string name, IDialogParameters parameters, Action<IDialogResult> callback)
+        {
+            if (!(dialogService is MaterialDialogService materialDialogService))
+                throw new NullReferenceException("DialogService must be a MaterialDialogService");
+
+            materialDialogService.ShowDialogHost(name, parameters, callback);
+        }
+
+        /// <summary>
+        /// Shows a modal dialog using a <see cref="MaterialDesignThemes.Wpf.DialogHost"/>.
+        /// </summary>
+        /// <param name="dialogService"></param>
+        /// <param name="name">The name of the dialog to show.</param>
+        /// <param name="parameters">The parameters to pass to the dialog.</param>
+        /// <param name="callback">The action to perform when the dialog is closed.</param>
+        /// <param name="windowName">The name of the <see cref="MaterialDesignThemes.Wpf.DialogHost"/> that will contain the dialog control</param>
+        /// <exception cref="NullReferenceException">Thrown when the dialog service is not a MaterialDialogService</exception>
+        public static void ShowDialogHost(this IDialogService dialogService, string name,
+            IDialogParameters parameters, Action<IDialogResult> callback, string windowName)
+        {
+            if (!(dialogService is MaterialDialogService materialDialogService))
+                throw new NullReferenceException("DialogService must be a MaterialDialogService");
+
+            materialDialogService.ShowDialogHost(name, windowName, parameters, callback);
+        }
+    }
+}
---
+++ src/Prism.Plugin.MaterialDesign/MaterialDialogService.cs
@@ -0,0 +1,96 @@
+﻿using Prism.Ioc;
+using Prism.Mvvm;
+using System.Windows;
+using System;
+using System.Windows.Interop;
+using System.Windows.Threading;
+using MaterialDesignThemes.Wpf;
+
+// ReSharper disable once CheckNamespace
+namespace Prism.Services.Dialogs
+{
+    public class MaterialDialogService : DialogService
+    {
+        private readonly IContainerExtension _containerExtension;
+
+        public MaterialDialogService(IContainerExtension containerExtension) : base(containerExtension)
+        {
+            _containerExtension = containerExtension;
+        }
+
+        public void ShowDialogHost(string name, IDialogParameters parameters, Action<IDialogResult> callback) =>
+            ShowDialogHost(name, null, parameters, callback);
+
+        public void ShowDialogHost(string name, string dialogHostName, IDialogParameters parameters, Action<IDialogResult> callback)
+        {
+            if (parameters == null)
+                parameters = new DialogParameters();
+            
+            var content = _containerExtension.Resolve<object>(name);
+            if (!(content is FrameworkElement dialogContent))
+            {
+                throw new NullReferenceException("A dialog's content must be a FrameworkElement");
+            }
+            
+            AutowireViewModel(dialogContent);
+
+            if (!(dialogContent.DataContext is IDialogAware dialogAware))
+            {
+                throw new ArgumentException("A dialog's ViewModel must implement IDialogAware interface");
+            }
+
+            var openedEventHandler = new DialogOpenedEventHandler((sender, args) =>
+            {
+                dialogAware.OnDialogOpened(parameters);
+            });
+            var closedEventHandler = new DialogClosedEventHandler((sender, args) =>
+            {
+                dialogAware.OnDialogClosed();
+            });
+            
+            dialogAware.RequestClose += res =>
+            {
+                if (DialogHost.IsDialogOpen(dialogHostName))
+                {
+                    callback(res);
+                    DialogHost.Close(dialogHostName);
+                }
+            };
+
+            var dispatcherFrame = new DispatcherFrame();
+            if (dialogHostName == null)
+            {
+                _ = DialogHost.Show(dialogContent, openedEventHandler, null, closedEventHandler)
+                    .ContinueWith(_ => dispatcherFrame.Continue = false);;
+            }
+            else
+            {
+                _ = DialogHost.Show(dialogContent, dialogHostName, openedEventHandler, null, closedEventHandler)
+                    .ContinueWith(_ => dispatcherFrame.Continue = false);
+            }
+
+            try
+            {
+                // tell users we're going modal
+                ComponentDispatcher.PushModal();
+ 
+                Dispatcher.PushFrame(dispatcherFrame);
+            }
+            finally
+            {
+                // tell users we're going non-modal
+                ComponentDispatcher.PopModal();
+            }
+
+            dialogAware.RequestClose -= callback;
+        }
+
+        private static void AutowireViewModel(object viewOrViewModel)
+        {
+            if (viewOrViewModel is FrameworkElement view && view.DataContext is null && ViewModelLocator.GetAutoWireViewModel(view) is null)
+            {
+                ViewModelLocator.SetAutoWireViewModel(view, true);
+            }
+        }
+    }
+}
