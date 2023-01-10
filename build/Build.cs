ede3d66163d51d68134a8be5a7817383a5b0cd83
Federico Ruggiero <feddstylez@gmail.com>
2023-01-10 01:33:43 +0100

    Scaffold build with nuke.build

---
+++ build/Build.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.NerdbankGitVersioning;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.FileSystemTasks;
+
+class Build : NukeBuild
+{
+    static AbsolutePath SourceDirectory => RootDirectory / "src";
+    static AbsolutePath TestsDirectory => RootDirectory / "test";
+    static AbsolutePath OutputDirectory => RootDirectory / "output";
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter("Package description")]
+    readonly string PackageDescription;
+
+    [Parameter("Authors")]
+    readonly string Authors;
+
+    [Parameter("Solution")]
+    readonly string Solution;
+
+    [NerdbankGitVersioning]
+    readonly NerdbankGitVersioning NerdbankVersioning;
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+        });
+
+    Target CleanOutput => _ => _
+        .Before(Pack)
+        .Executes(() =>
+        {
+            EnsureCleanDirectory(OutputDirectory);
+        });
+    
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetRestore();
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetBuild(_ =>
+            {
+                _ = _.SetProjectFile(Solution)
+                    .SetConfiguration(Configuration)
+                    .SetAuthors(Authors)
+                    .SetAssemblyVersion(NerdbankVersioning.AssemblyVersion)
+                    .SetFileVersion(NerdbankVersioning.AssemblyFileVersion)
+                    .SetInformationalVersion(NerdbankVersioning.AssemblyInformationalVersion);
+
+                return _;
+            });
+        });
+
+    Target Test => _ => _
+        .After(Compile)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetTest(_ => _
+                .SetConfiguration(Configuration)
+                .SetFilter("FullyQualifiedName!~IntegrationTests")
+                .EnableNoBuild());
+        });
+
+    Target Pack => _ => _
+        .After(Compile)
+        .DependsOn(CleanOutput)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetPack(_ => _
+                .SetOutputDirectory(OutputDirectory)
+                .SetVersion(NerdbankVersioning.NuGetPackageVersion)
+                .SetDescription(PackageDescription)
+                .SetConfiguration(Configuration)
+                .SetProperties(new Dictionary<string, object>
+                {
+                    {"Version", NerdbankVersioning.NuGetPackageVersion},
+                    {"Description", PackageDescription},
+                    {"Authors", Authors},
+                })
+                .EnableNoBuild());
+        });
+
+    Target Deploy => _ => _
+        .After(Pack)
+        .Executes(() =>
+        {
+            FileSystemTasks.CopyDirectoryRecursively(OutputDirectory, @"D:\local_packages", DirectoryExistsPolicy.Merge,
+                FileExistsPolicy.Overwrite);
+        });
+}
